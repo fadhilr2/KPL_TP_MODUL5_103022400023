3f90ff533289164f0a3b8046530633e1b8ad1ff6
fadhilr2 <86996097+fadhilr2@users.noreply.github.com>
2026-03-29 22:04:59 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T d){
+        this.data = d;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+
+}
+
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("Masukkan NIM: ");
+        string input = Console.ReadLine();
+        DataGeneric<string> dataGeneric = new DataGeneric<string>(input);
+
+        dataGeneric.PrintData();
+    }
+}
